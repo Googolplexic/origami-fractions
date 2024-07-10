64b11826eeea6cc95eda1184cef7576d052fe649
Googolplexic <colemancflai@yahoo.com>
2024-07-09 20:09:11 -0700

    Scaling SVG

--- CP_Creation.js
+++ CP_Creation.js
@@ -1,17 +1,19 @@
-
 class CP {
     #ns = 'http://www.w3.org/2000/svg';
     #square;
-    #div;
-    constructor(size, div) {
-        this.#div = div;
+    #svg;
+    constructor(size, svg) {
+        this.#svg = svg;
         this.size = size;
         this.#square = this.#createCentredSquare(size);
-        this.#div.appendChild(this.#square);
+        this.#svg.appendChild(this.#square);
+        console.log("done");
     }
+
     drawCornerDiagonal() {
         this.#drawDiagonalInSquare([0, 0], [1, 1], this.#square, 'grey', 'solid');
     }
+
     drawCrease(a, b, dir, type) {
         let colour;
         switch (dir) {
@@ -28,57 +30,38 @@ class CP {
         this.#drawDiagonalInSquare(a, b, this.#square, colour, type);
     }
 
-
     #createCentredSquare(size) {
         const square = document.createElementNS(this.#ns, 'rect');
+        const vBox = this.#svg.getAttribute('viewBox').split(' ').map(Number);
+        const vBoxSize = Math.min(vBox[2], vBox[3]);
         square.setAttribute('width', size);
         square.setAttribute('height', size);
-        square.setAttribute('x', this.#div.getAttribute('width') / 2 - square.getAttribute('width') / 2);
-        square.setAttribute('y', this.#div.getAttribute('height') / 2 - square.getAttribute('height') / 2);
+        square.setAttribute('x', (vBoxSize - size) / 2);  // Centering within the viewBox
+        square.setAttribute('y', (vBoxSize - size) / 2);  // Centering within the viewBox
         square.setAttribute('fill', 'none');
         square.setAttribute('stroke', 'gray');
         square.setAttribute('size', size);
         return square;
     }
 
-    #getCornerCoords(quadrant, square) {
-
-        const width = parseInt(this.#div.getAttribute('width'), 10);
-        const height = parseInt(this.#div.getAttribute('height'), 10);
-
-        switch (quadrant) {
-            case 1:
-                return [(width + square.getAttribute('size')) / 2, (height - square.getAttribute('size')) / 2];
-            case 2:
-                return [(width - square.getAttribute('size')) / 2, (height - square.getAttribute('size')) / 2];
-            case 3:
-                return [(width - square.getAttribute('size')) / 2, (height + square.getAttribute('size')) / 2];
-            case 4:
-                return [(width + square.getAttribute('size')) / 2, (height + square.getAttribute('size')) / 2];
-        }
-    }
-
-    #getCoordsInSquare(a, square) {
-        let x = this.#getCornerCoords(3, square)[0] + a[0] * square.getAttribute('size');
-        let y = this.#getCornerCoords(2, square)[1] + (1 - a[1]) * square.getAttribute('size');
-        console.log(this.#getCornerCoords(2, square)[1]);
-        return [x, y];
-    }
-
     #drawDiagonalInSquare(a, b, square, colour, type) {
         const line = document.createElementNS(this.#ns, 'line');
-        let [x1, y1] = this.#getCoordsInSquare(a, square);
-        let [x2, y2] = this.#getCoordsInSquare(b, square);
+        const size = parseFloat(square.getAttribute('size'));
+
+        // Calculate relative coordinates within the viewBox
+        let x1 = parseFloat(square.getAttribute('x')) + a[0] * size;
+        let y1 = parseFloat(square.getAttribute('y')) + a[1] * size;
+        let x2 = parseFloat(square.getAttribute('x')) + b[0] * size;
+        let y2 = parseFloat(square.getAttribute('y')) + b[1] * size;
+
         line.setAttribute('x1', x1);
         line.setAttribute('y1', y1);
         line.setAttribute('x2', x2);
         line.setAttribute('y2', y2);
-        console.log(x1, " ", y1, " ", x2, " ", y2);
         line.setAttribute('stroke', colour);
-        line.setAttribute('stroke-width', 2);
-        if (type == 'dashed') { line.setAttribute('stroke-dasharray', '5,5'); }
-        this.#div.appendChild(line);
-
+        if (type === 'dashed') {
+            line.setAttribute('stroke-dasharray', '5,5');
+        }
+        this.#svg.appendChild(line);
     }
-
 }
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function calculate() {
     var div = document.getElementById('svgCanvas');
-    const size = Math.max(div.getAttribute('width'), div.getAttribute('height')) * 0.8;
+    var vBox = div.getAttribute('viewBox').split(' ').map(Number);
+    const size = Math.min(vBox[2], vBox[3]) * 0.8;
     fractCP = new CP(size, div);
     fractCP.drawCornerDiagonal();
     fractCP.drawCrease([0, 1 / 3], [0.3, 0.8], 'M', 'Solid');
+    
 }
