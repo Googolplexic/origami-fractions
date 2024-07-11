bcf1506e782c6d6f113d5a3307201e41fa1c0b7e
Googolplexic <colemancflai@yahoo.com>
2024-07-11 15:29:15 -0700

    Added a sort to shapeArray to make the images nicer

--- classes.js
+++ classes.js
@@ -10,7 +10,8 @@ class CP {
     }
 
     drawCP() {
-        this.#svg.appendChild(this.#square);
+        this.#shapeArray.push(this.#square);
+        this.#shapeArray.sort(this.#sort);
         for (const shape of this.#shapeArray) {
             this.#svg.appendChild(shape);
         }
@@ -51,53 +52,65 @@ class CP {
         }
 
     }
+    #sort(a, b) {
+        const order = {
+            'line': 1,
+            'rect': 2,
+            'circle': 3
+        };
 
-    #createCentredSquare(size) {
-        const square = document.createElementNS(this.#ns, 'rect');
-        const vBox = this.#svg.getAttribute('viewBox').split(' ').map(Number);
-        const vBoxSize = Math.min(vBox[2], vBox[3]);
-        square.setAttribute('width', size);
-        square.setAttribute('height', size);
-        square.setAttribute('x', (vBoxSize - size) / 2);
-        square.setAttribute('y', (vBoxSize - size) / 2);
-        square.setAttribute('fill', 'none');
-        square.setAttribute('stroke', 'gray');
-        square.setAttribute('size', size);
-        return square;
+        let orderA = order[a];
+        let orderB = order[b];
+
+        return orderA - orderB;
     }
+}
+#createCentredSquare(size) {
+    const square = document.createElementNS(this.#ns, 'rect');
+    const vBox = this.#svg.getAttribute('viewBox').split(' ').map(Number);
+    const vBoxSize = Math.min(vBox[2], vBox[3]);
+    square.setAttribute('width', size);
+    square.setAttribute('height', size);
+    square.setAttribute('x', (vBoxSize - size) / 2);
+    square.setAttribute('y', (vBoxSize - size) / 2);
+    square.setAttribute('fill', 'none');
+    square.setAttribute('stroke', 'gray');
+    square.setAttribute('size', size);
+    return square;
+}
 
-    #createLineInSquare(a, b, square, colour, type) {
-        const line = document.createElementNS(this.#ns, 'line');
-        const size = parseFloat(square.getAttribute('size'));
+#createLineInSquare(a, b, square, colour, type) {
+    const line = document.createElementNS(this.#ns, 'line');
+    const size = parseFloat(square.getAttribute('size'));
 
-        let x1 = parseFloat(square.getAttribute('x')) + a[0] * size;
-        let y1 = parseFloat(square.getAttribute('y')) + size - a[1] * size;
-        let x2 = parseFloat(square.getAttribute('x')) + b[0] * size;
-        let y2 = parseFloat(square.getAttribute('y')) + size - b[1] * size;
+    let x1 = parseFloat(square.getAttribute('x')) + a[0] * size;
+    let y1 = parseFloat(square.getAttribute('y')) + size - a[1] * size;
+    let x2 = parseFloat(square.getAttribute('x')) + b[0] * size;
+    let y2 = parseFloat(square.getAttribute('y')) + size - b[1] * size;
 
-        line.setAttribute('x1', x1);
-        line.setAttribute('y1', y1);
-        line.setAttribute('x2', x2);
-        line.setAttribute('y2', y2);
-        line.setAttribute('stroke', colour);
-        if (type === 'dashed') {
-            line.setAttribute('stroke-dasharray', '2%');
-        }
-        this.#shapeArray.push(line);
+    line.setAttribute('x1', x1);
+    line.setAttribute('y1', y1);
+    line.setAttribute('x2', x2);
+    line.setAttribute('y2', y2);
+    line.setAttribute('stroke', colour);
+    if (type === 'dashed') {
+        line.setAttribute('stroke-dasharray', '2%');
     }
+    this.#shapeArray.push(line);
+}
 
-    createCircleInSquare(loc, square, radius, colour) {
-        const circle = document.createElementNS(this.#ns, 'circle');
-        const size = parseFloat(square.getAttribute('size'));
+createCircleInSquare(loc, square, radius, colour) {
+    const circle = document.createElementNS(this.#ns, 'circle');
+    const size = parseFloat(square.getAttribute('size'));
 
-        let x = parseFloat(square.getAttribute('x')) + loc[0] * size;
-        let y = parseFloat(square.getAttribute('y')) + size - loc[1] * size;
-        circle.setAttribute('cx', x);
-        circle.setAttribute('cy', y);
-        circle.setAttribute('r', radius);
-        circle.setAttribute('fill', colour);
-        circle.setAttribute('stroke', colour);
-        this.#shapeArray.push(circle);
-    }
+    let x = parseFloat(square.getAttribute('x')) + loc[0] * size;
+    let y = parseFloat(square.getAttribute('y')) + size - loc[1] * size;
+    circle.setAttribute('cx', x);
+    circle.setAttribute('cy', y);
+    circle.setAttribute('r', radius);
+    circle.setAttribute('fill', colour);
+    circle.setAttribute('stroke', colour);
+    this.#shapeArray.push(circle);
+}
 }
 
