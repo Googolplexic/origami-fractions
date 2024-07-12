d21c3a5401255e7975250c88883ead59562a54d1
Googolplexic <colemancflai@yahoo.com>
2024-07-11 20:55:24 -0700

    Moved the getNext and getPrevious functions out to reuse in preparation for more generation methods

--- svgTools.js
+++ svgTools.js
@@ -20,48 +20,46 @@ function crossingDiagonals(svg, previous, next, event) {
     event.preventDefault();
     const [a, b, size] = setupSVG(svg, previous, next, event);
     if (a === 0) { return; }
-    console.log("sdf");
     CP_array = generateCrossingDiagonals(svg, size, a, b);
     map.set(svg.id, CP_array);
     svg.innerHTML = "";
     CP_array[0].drawCP();
     svg.setAttribute('index', 0);
 
-    function getPrevious() {
-        let array = map.get(svg.id);
 
-        let idx = (svg.getAttribute('index'));
-        if (idx > 0) {
-            idx--;
-            svg.setAttribute('index', idx);
-        }
-        if (idx >= 0 && array.length > 0) {
-            svg.innerHTML = "";
-            array[idx].drawCP();
-        }
-    }
-
-    function getNext() {
-        let array = map.get(svg.id);
-        let idx = svg.getAttribute('index');
-
-        if (idx < array.length - 1) {
-            idx++;
-            svg.setAttribute('index', idx);
-        }
-        if (idx >= 0 && array.length > 0) {
-            svg.innerHTML = "";
-            array[idx].drawCP();
-        }
-    }
-
-    previous.onclick = function (event) {
-        event.preventDefault();
-        getPrevious();
+    previous.onclick = function () {
+        getPrevious(svg);
     };
-    next.onclick = function (event) {
-        event.preventDefault();
-        getNext();
+    next.onclick = function () {
+        getNext(svg);
     };
 }
 
+
+function getPrevious(svg) {
+    let array = map.get(svg.id);
+
+    let idx = (svg.getAttribute('index'));
+    if (idx > 0) {
+        idx--;
+        svg.setAttribute('index', idx);
+    }
+    if (idx >= 0 && array.length > 0) {
+        svg.innerHTML = "";
+        array[idx].drawCP();
+    }
+}
+
+function getNext(svg) {
+    let array = map.get(svg.id);
+    let idx = svg.getAttribute('index');
+
+    if (idx < array.length - 1) {
+        idx++;
+        svg.setAttribute('index', idx);
+    }
+    if (idx >= 0 && array.length > 0) {
+        svg.innerHTML = "";
+        array[idx].drawCP();
+    }
+}
